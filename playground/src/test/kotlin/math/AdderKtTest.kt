4664a1e123243c84b796f0cc362054d7b25eb047
Johnny Da Costa <johnny.dacosta@stemys.io>
2024-05-02 08:21:40 +0200

    chore: add new test that add two double previously parse from json to object

--- playground/src/test/kotlin/math/AdderKtTest.kt
+++ playground/src/test/kotlin/math/AdderKtTest.kt
@@ -1,8 +1,9 @@
 package math
 
 import ch.jdc.math.Add
-import org.junit.jupiter.api.Assertions.*
-
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
 
@@ -11,6 +12,23 @@ class AdderKtTest {
     @Test
     fun `when add two double should get correct result`() {
         assertEquals(0.3, Add(0.1, 0.2))
+    }
+
+    @Test
+    fun `when add two double parse from json should get correct result`() {
+        @Serializable
+        data class Payload(val a: Double, val b: Double)
+
+        val data = """
+            {
+                "a": 0.1,
+                "b": 0.2
+            }
+        """
+
+        val values = Json.decodeFromString<Payload>(data)
+
+        assertEquals(0.3, Add(values.a, values.b))
     }
 
     @Test
